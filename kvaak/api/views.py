9fa6e19722d37184f481aed8b94ab8cd07b074cf
UFo_x <tero.jaakkola1@gmail.com>
2018-01-03 18:51:37 +0200

    fix typo

--- kvaak/api/views.py
+++ kvaak/api/views.py
@@ -19,4 +19,4 @@ def species_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
