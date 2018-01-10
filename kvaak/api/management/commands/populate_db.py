5d628144992d589790459af39ee7980005916ab5
UFo_x <tero.jaakkola1@gmail.com>
2018-01-10 20:46:38 +0200

    Create wanted amount of sightings

--- kvaak/api/management/commands/populate_db.py
+++ kvaak/api/management/commands/populate_db.py
@@ -1,34 +1,59 @@
 from django.core.management.base import BaseCommand
-from django.core.exceptions import ObjectDoesNotExist
 from ...models import Sighting
 from ...models import Species
 
 
 class Command(BaseCommand):
-    args = '<foo bar ...>'
-    help = 'our help string comes here'
+    help = 'Populate database with dummy data. \
+            Amount of Sightings given as argument. \
+            Creates 6 Sightings if argument not given. \
+             \n ex. python manage.py populate_db 15'
 
-    species = {'mallard', 'redhead', 'gadwall', 'canvasback', 'lesser', 'scaup'}
-    dates = {'1990-12-23T23:33:00Z', '2015-01-05T17:20:59Z', '2016-09-11T18:15:15Z', '2017-04-30T07:00:30Z', '2018-01-16T11:00:42Z', '2000-03-17T16:59:59Z'}
-    count = {1, 5, 22 , 223, 1000, 44}
+    species = {
+        'mallard',
+        'redhead',
+        'gadwall',
+        'canvasback',
+        'lesser',
+        'scaup'
+        }
+    dates = {
+        '1990-12-23T23:33:00Z',
+        '2015-01-05T17:20:59Z',
+        '2016-09-11T18:15:15Z',
+        '2017-04-30T07:00:30Z',
+        '2018-01-16T11:00:42Z',
+        '2000-03-17T16:59:59Z'
+        }
+    count = {1, 5, 22, 223, 1000, 44}
 
-    def _create_sightings(self):
-        for bird, date, count in zip(self.species, self.dates, self.count):
+    def _add_species(self, species):
+        for bird in species:
             try:
                 species = Species.objects.get(name=bird)
             except Species.DoesNotExist:
                 species = Species(name=bird)
                 species.save()
-            sighting = Sighting(date_time=date,
+
+    def _create_sightings(self, count):
+        # TODO: Create random data
+        self._add_species(self.species)
+        for i in range(count):
+            sighting = Sighting(date_time='1990-12-23T23:33:00Z',
                                 description='wow such bird',
-                                species=Species.objects.get(name=bird),
-                                count=count
+                                species=Species.objects.get(name="mallard"),
+                                count=3
                                 )
+            self.stdout.write(self.style.SUCCESS('Successfully added sighting %s!' % sighting.species))
             sighting.save()
 
-    def _create_species(self):
-        species = Species(name='tweeter')
-        species.save()
+    def add_arguments(self, parser):
+        # https://docs.python.org/3/library/argparse.html
+        parser.add_argument('sightings', nargs='*', type=int, default=5)
 
     def handle(self, *args, **options):
-        self._create_sightings()
+        try:
+            for sightings_count in options['sightings']:
+                self._create_sightings(sightings_count)
+        except TypeError:
+            self._create_sightings(6)
